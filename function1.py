912ee2e7ac9c0a136f2aa78944302f8f33eef79c
jeongyeol-python2 <jeongyeolpython2@gmail.com>
2024-05-07 17:34:51 +0900

    함수연습2

--- function1.py
+++ function1.py
@@ -1,4 +1,40 @@
 # function1.py
+
+#함수 이름 해석 규칙(L-local, G-grobal, B-built in - 내장함수)
+#전역변수가 있는 경우
+x=1
+def func(a):
+    return a+x
+
+#호출
+print(func(1))
+
+#지역변수가 있는 경우
+def func2(a):
+    x=5
+    return a+x
+
+#호출
+print(func2(1))
+
+#함수의 기본값
+def times(a=10, b=20):
+    return a*b
+
+#호출
+print(times())
+print(times(5))
+print(times(5,6))
+
+#키워드 인자방식 (상세 기술)
+def connectURI(server, port):
+    strURL = "https://" + server + ":" + port
+    return strURL
+
+#호출
+print(connectURI("multi.com", "80"))
+print(connectURI(port="80", server="naver.com"))
+
 #불변형식
 a = 1.2
 print("a id:", id(a) )
