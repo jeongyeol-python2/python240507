04b2b812fcee531c8abfe5778faef5e236ae55c8
jeongyeol-python2 <jeongyeolpython2@gmail.com>
2024-05-08 11:27:31 +0900

    클래스 상속 연습1

--- 상속01.py
+++ 상속01.py
@@ -1,3 +1,4 @@
+#부모 클래스 정의
 class Person:
     def __init__(self, name, phoneNumber):
         self.name = name
@@ -6,17 +7,23 @@ class Person:
     def printInfo(self):
         print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
 
+#자식 클래스 정의
 class Student(Person):
+    #재정의(Override)
     def __init__(self, name, phoneNumber, subject, studentID):
-        self.name = name
-        self.phoneNumber = phoneNumber
+        #부모 클래스 초기화 메서드 호출
+        Person.__init__(self, name, phoneNumber)
         self.subject = subject
         self.studentID = studentID
-
+    #상속 메서드 재정의
+    def printInfo(self):
+        print("Info(이름:{0}, 전번: {1})".format(self.name, self.phoneNumber))
+        print("Info(학과:{0}, 헉번: {1})".format(self.subject, self.studentID))
 
 p = Person("전우치", "010-222-1234")
 s = Student("이순신", "010-111-1234", "컴공", "991122")
 print(p.__dict__)
 print(s.__dict__)
 
-
+p.printInfo()
+s.printInfo()
