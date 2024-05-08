d34c7f28b5151921bef75f2a3a792d65cd33bfe8
jeongyeol-python2 <jeongyeolpython2@gmail.com>
2024-05-08 09:37:07 +0900

    조건 및 반복문 연습 1

--- if_else.py
+++ if_else.py
@@ -1,4 +1,6 @@
-score = int(input('Input Score: '))
+#분기 반복 구문
+score = 99 #int(input("점수를 입력:"))
+
 if 90 <= score <= 100:
     grade = "A"
 elif 80 <= score < 90:
@@ -11,3 +13,20 @@ else:
     grace = "F"
     
 print("Grade is " + grade)
+
+value = 5
+while value > 0:
+    print(value)
+    value -= 1
+
+lst = [100, "apple", 3.14]
+for item in lst:
+    print(item)
+
+fruits = {"apple": 100, "kiwi":200}
+for k,v in fruits.items():
+    print(k, v)
+
+print("----range() 함수----")
+print(list(range(2000,2025)))
+print(list(range(1,32)))
