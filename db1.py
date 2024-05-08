77ceb5a141944b08ab4e52f6d22f172a32f6bc0a
jeongyeol-python2 <jeongyeolpython2@gmail.com>
2024-05-08 17:48:22 +0900

    데이터베이스 연습2

--- db1.py
+++ db1.py
@@ -14,8 +14,17 @@ cur.execute("CREATE TABLE PhoneBook (Name text, PhoneNum text);")
 #입력
 cur.execute("insert into PhoneBook values ('전우치', '010-222-0001');")
 
+#입력 파라미터 처리
+name = "홍길동"
+phoneNumber = "010-123-4567"
+cur.execute("insert into PhoneBook values (?, ?);", (name, phoneNumber))
+
+#한번에 2개를 입력(다중 행 입력)
+dataList = (("이순신", "010-111-1111"), ("박문수", "010-222-2222"))
+cur.executemany("insert into PhoneBook values (?, ?)", dataList)
+
 #검색
 cur.execute("select * from PhoneBook;")
 
 for row in cur:
-    print(row)
+    print(row[0], row[1])
